b214a24997ecd4ebb31d3f7b51ad68d5915c7377
Raunak0808 <82041818+Raunak0808@users.noreply.github.com>
2024-10-18 13:14:06 +0530

    Add files via upload

---
+++ project1.py
@@ -0,0 +1,9 @@
+#Project - 1 - Band Name Generator
+
+print("*****Project - 1 - Band Name Generator*****")
+print("Welcome to Band Name Generator")
+
+city_name = input("What's the name of city you grew up in?: ")
+pet_name = input("What's the pet's name?: ")
+
+print(city_name,pet_name)
